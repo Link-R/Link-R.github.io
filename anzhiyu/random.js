17394e96439b4d96b38bd5192c7f56a071860405
FuFu <2361325611@qq.com>
2025-04-18 21:07:53 +0800

    Site updated: 2025-04-18 21:07:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/13/hello-world/","2025/04/18/hello-world1/"];function toRandomPost(){
+var posts=["2025/04/13/hello-world/","2025/04/18/hello-world1/","2025/04/18/welcome/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
